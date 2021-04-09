5d0e22ad37ff2b4ef02b8032aa891a3fd4fc255e
wit <witwitchayakarn@gmail.com>
2021-04-09 18:15:52 +0700

    Try Wordpress Gutenberg without success!

--- packages/mobile/src/components/Main.js
+++ packages/mobile/src/components/Main.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSafeAreaFrame } from 'react-native-safe-area-context';
+import { registerCoreBlocks } from '@wordpress/block-library';
 
 import { LG_WIDTH } from '../types/const';
 
@@ -11,10 +12,19 @@ import MoveToPopup from './MoveToPopup';
 import ConfirmDeletePopup from './ConfirmDeletePopup';
 import SettingsPopup from './SettingsPopup';
 
+let didRegisterCoreBlocks = false;
+
 const Main = () => {
 
   const { width: safeAreaWidth } = useSafeAreaFrame();
   const panel = safeAreaWidth < LG_WIDTH ? <NavPanel /> : <ColsPanel />;
+
+  useEffect(() => {
+    if (!didRegisterCoreBlocks) {
+      registerCoreBlocks();
+      didRegisterCoreBlocks = true;
+    }
+  }, []);
 
   return (
     <React.Fragment>
--- packages/mobile/src/components/NoteEditorEditor.js
+++ packages/mobile/src/components/NoteEditorEditor.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { View, TextInput } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  BlockEditorProvider, BlockList, WritingFlow, ObserveTyping,
-} from '@wordpress/block-editor';
-import { SlotFillProvider, Popover } from '@wordpress/components';
-import { registerCoreBlocks } from '@wordpress/block-library';
+
+import '@wordpress/edit-post/src/store';
+import '@wordpress/format-library';
+import { SlotFillProvider } from '@wordpress/components';
+import { BlockEditorProvider, BlockList } from '@wordpress/block-editor';
+import EditorHeader from '@wordpress/edit-post/src/components/header';
 
 import { updateEditorFocused, updateEditorContent } from '../actions';
 import { NEW_NOTE, ADDED } from '../types/const';
@@ -22,6 +23,17 @@ const NoteEditorEditor = (props) => {
   const dispatch = useDispatch();
 
   const [blocks, updateBlocks] = useState([]);
+
+  const editorSettings = useMemo(() => {
+    return {
+      hasFixedToolbar: true,
+      hasUploadPermissions: true,
+      capabilities: {
+        mentions: false,
+        xposts: false,
+      },
+    };
+  }, []);
 
   const onTitleInputChange = (e) => {
     if (!isFocused) dispatch(updateEditorFocused(true));
@@ -47,17 +59,16 @@ const NoteEditorEditor = (props) => {
     if (note.id === NEW_NOTE) setTimeout(() => titleInput.current.focus(), 1);
   }, [note]);
 
-  useEffect(() => {
-    registerCoreBlocks();
-  }, []);
-
   return (
     <View style={tailwind('px-3 py-3 flex-1')}>
       <TextInput ref={titleInput} onChange={onTitleInputChange} style={tailwind('w-full flex-grow-0 flex-shrink-0 text-sm text-gray-600 font-normal py-2 px-3 border-gray-300 rounded-md')} placeholder="Note Title" placeholderTextColor="rgba(107, 114, 128, 1)" value={title} editable={note.id === NEW_NOTE || note.status === ADDED} />
       <View style={tailwind('h-10 border-b border-gray-200 py-3 flex-grow-0 flex-shrink-0 flex items-center justify-between')} />
-      <BlockEditorProvider value={blocks} onInput={(blocks) => updateBlocks(blocks)} onChange={(blocks) => updateBlocks(blocks)}>
-        <BlockList />
-      </BlockEditorProvider>
+      <SlotFillProvider>
+        <BlockEditorProvider value={blocks} onInput={(b) => updateBlocks(b)} onChange={(b) => updateBlocks(b)} settings={editorSettings}>
+          <BlockList withFooter={false} />
+          <EditorHeader />
+        </BlockEditorProvider>
+      </SlotFillProvider>
     </View>
   );
 };
