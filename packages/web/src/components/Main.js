62112006b7e8ce5196e14e99c1e4eae82078fbec
wit <witwitchayakarn@gmail.com>
2021-07-30 16:08:40 +0700

    Fix overflow-hidden on body only when rendering Main

--- packages/web/src/components/Main.js
+++ packages/web/src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { LG_WIDTH } from '../types/const';
 
@@ -16,6 +16,18 @@ import AlertScreenRotationPopup from './AlertScreenRotationPopup';
 const Main = () => {
 
   const { width: safeAreaWidth } = useSafeAreaFrame();
+
+  useEffect(() => {
+    // Need to add class name: overflow-hidden to <body>
+    //   so that CKEditor image toolbar popup doesn't make the body scrollable
+    const body = document.body || document.getElementsByTagName('body')[0];
+    body.classList.add('overflow-hidden');
+
+    return () => {
+      body.classList.remove('overflow-hidden');
+    };
+  }, []);
+
   const panel = safeAreaWidth < LG_WIDTH ? <NavPanel /> : <ColsPanel />;
 
   return (
