8b4bbcfdc46838dee2c11c680c92fe3fde82ae58
wit <witwitchayakarn@gmail.com>
2021-09-24 14:23:21 +0700

    Fix misplaced error msg of settings lists in both Android and iOS

--- packages/mobile/src/components/SettingsPopupLists.js
+++ packages/mobile/src/components/SettingsPopupLists.js
@@ -325,7 +325,7 @@ const _ListNameEditor = (props) => {
       </TouchableOpacity>}
       <View style={tailwind('flex-grow flex-shrink')}>
         <TextInput ref={input} onFocus={onInputFocus} onBlur={onInputBlur} onChange={onInputChange} onSubmitEditing={onInputKeyPress} style={tailwind('px-0 py-1 w-full bg-white text-base leading-5 text-gray-600 font-normal border-0')} placeholder="Create new list" value={state.value} editable={!(isBusy || doRetry)} />
-        <Text style={[tailwind('absolute left-0 right-0 text-sm text-red-600 font-medium leading-5'), { bottom: -8 }]} numberOfLines={1} ellipsizeMode="tail">{errMsg}</Text>
+        <Text style={[tailwind('absolute left-0 right-0 text-sm text-red-600 font-medium leading-5'), { bottom: Platform.OS === 'ios' ? -12 : -8 }]} numberOfLines={1} ellipsizeMode="tail">{errMsg}</Text>
       </View>
       {state.mode === MODE_EDIT && <TouchableOpacity onPressIn={onOkBtnPress} onPress={onOkBtnClick} style={tailwind('flex-grow-0 flex-shrink-0 justify-center items-center w-10 h-10')}>
         <Svg style={tailwind('text-gray-500 font-normal')} width={16} height={12} viewBox="0 0 14 10" fill="currentColor">
