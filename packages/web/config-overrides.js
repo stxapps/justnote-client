9d853746b7cccb175d51574fa6a750fb5874c2fa
witwitchayakarn <witwitchayakarn@gmail.com>
2022-01-10 11:23:23 +0700

    Upgrade web to CRA 5 and Tailwind 3

---
+++ packages/web/config-overrides.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    process: require.resolve('process/browser'),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert'),
+    buffer: require.resolve('buffer'),
+    util: require.resolve('util'),
+  };
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  );
+
+  return config;
+};
--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,19 +1,15 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        green: colors.green,
       },
       minWidth: {
         '28': '7rem',
@@ -28,18 +24,6 @@ module.exports = {
       cursor: {
         resize: 'col-resize',
       },
-    },
-  },
-  variants: {
-    extend: {
-      textColor: ['group-focus', 'focus-visible'],
-      backgroundColor: ['group-focus', 'focus-visible'],
-      borderColor: ['group-focus', 'focus-visible'],
-      ringColor: ['group-hover', 'group-focus', 'hover', 'focus-visible'],
-      ringOffsetColor: ['group-hover', 'group-focus', 'hover', 'focus-visible'],
-      ringOffsetWidth: ['group-hover', 'group-focus', 'hover', 'focus-visible'],
-      ringOpacity: ['group-hover', 'group-focus', 'hover', 'focus-visible'],
-      ringWidth: ['group-hover', 'group-focus', 'hover', 'focus-visible'],
     },
   },
   plugins: [
