e51874ad2ac517cf733e18c11d0c87b89e5172d5
wit <witwitchayakarn@gmail.com>
2022-11-16 15:30:55 +0700

    Update stale error message

--- packages/web/src/components/StaleErrorPopup.js
+++ packages/web/src/components/StaleErrorPopup.js
@@ -33,7 +33,7 @@ const StaleErrorPopup = () => {
 
   return (
     <div className={tailwind('fixed inset-x-0 top-14 z-40 flex items-start justify-center md:top-0')}>
-      <div className={tailwind('relative m-4 rounded-md bg-red-50 p-4 shadow-lg')}>
+      <div className={tailwind('relative m-4 max-w-[26rem] rounded-md bg-red-50 p-4 shadow-lg')}>
         <div className={tailwind('flex')}>
           <div className={tailwind('flex-shrink-0')}>
             <svg className={tailwind('h-6 w-6 text-red-400')} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
@@ -42,7 +42,7 @@ const StaleErrorPopup = () => {
           </div>
           <div className={tailwind('ml-3 lg:mt-0.5')}>
             <h3 className={tailwind('text-left text-base font-medium text-red-800 lg:text-sm')}>Your notes are out of date!</h3>
-            <p className={tailwind('mt-2.5 text-sm text-red-700')}>Please refresh to update your notes to the latest version. <br className={tailwind('hidden sm:inline')} />If the problem persists, please <a className={tailwind('rounded underline hover:text-red-800 focus:outline-none focus:ring-2 focus:ring-red-700')} href={'/' + HASH_SUPPORT} target="_blank" rel="noreferrer">contact us</a>.</p>
+            <p className={tailwind('mt-2.5 text-sm text-red-700')}>Please refresh to update your notes to the latest version. Save your changes before. And if the problem persists, please <a className={tailwind('rounded underline hover:text-red-800 focus:outline-none focus:ring-2 focus:ring-red-700')} href={'/' + HASH_SUPPORT} target="_blank" rel="noreferrer">contact us</a>.</p>
             <div className={tailwind('mt-4')}>
               <div className={tailwind('-mx-2 -my-1.5 flex')}>
                 <button onClick={onRefreshBtnClick} className={tailwind('rounded-md bg-red-50 px-2 py-1.5 text-sm font-medium text-red-800 transition duration-150 ease-in-out hover:bg-red-100 focus:bg-red-100 focus:outline-none')}>Refresh</button>
