dd5e83996b1f7927adc0e1ad3b6a810a3d461f8b
wit <witwitchayakarn@gmail.com>
2023-04-03 16:17:14 +0700

    Display alert if sign up or sign in error

--- packages/mobile/src/actions/index.js
+++ packages/mobile/src/actions/index.js
@@ -212,7 +212,12 @@ export const signOut = () => async (dispatch, getState) => {
 };
 
 export const updateUserData = (data) => async (dispatch, getState) => {
-  await userSession.updateUserData(data);
+  try {
+    await userSession.updateUserData(data);
+  } catch (error) {
+    Alert.alert('Update user data failed!', `Please wait a moment and try again. If the problem persists, please contact us.\n\n${error}`);
+    return;
+  }
 
   const isUserSignedIn = await userSession.isUserSignedIn();
   if (isUserSignedIn) dispatch(updateUserSignedIn());
