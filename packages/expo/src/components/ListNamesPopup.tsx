63fcf7db3bb58f70fb7f28c083f12436b0656a1e
wit <witwitchayakarn@gmail.com>
2025-07-28 11:15:58 +0700

    Fix text height diff only on iPhone again

--- packages/expo/src/components/ListNamesPopup.tsx
+++ packages/expo/src/components/ListNamesPopup.tsx
@@ -221,10 +221,10 @@ const ListNamesPopup = () => {
   if (longestDisplayName.length > 26) popupWidth = 256;
   else if (longestDisplayName.length > 14) popupWidth = 208;
 
-  let popupHeight = 44 + (44 * maxChildrenSize) + 52;
+  let popupHeight = 44 + (44 * maxChildrenSize) + 48;
   if (popupHeight > safeAreaHeight - 16) {
     popupHeight = getLastHalfHeight(
-      Math.min(popupHeight, safeAreaHeight - 16), 44, 0, 52, 0.5
+      Math.min(popupHeight, safeAreaHeight - 16), 44, 0, 48, 0.5
     );
   }
 
@@ -306,9 +306,9 @@ const ListNamesPopup = () => {
             <ScrollView>{renderListNameBtns()}</ScrollView>
           </Animated.View>
         </View>
-        <View style={tailwind('h-13 flex-shrink-0 flex-grow-0 flex-row items-center justify-end border-t border-gray-200 px-3 blk:border-gray-600')}>
+        <View style={tailwind('h-12 flex-shrink-0 flex-grow-0 flex-row items-center justify-end border-t border-gray-200 px-3 blk:border-gray-600')}>
           <TouchableOpacity onPress={onMoveHereBtnClick} style={tailwind(`rounded-md border bg-white px-3 py-1 blk:bg-gray-800 ${moveHereDisabled ? 'border-gray-300 blk:border-gray-600' : 'border-gray-400 blk:border-gray-400'}`)} disabled={moveHereDisabled}>
-            <Text style={tailwind(`text-xs leading-4.5 font-normal ${moveHereDisabled ? 'text-gray-400 blk:text-gray-500' : 'text-gray-500 blk:text-gray-300'}`)}>{moveHereDisabled ? 'View only' : 'Move here'}</Text>
+            <Text style={tailwind(`text-xs font-normal ${moveHereDisabled ? 'text-gray-400 blk:text-gray-500' : 'text-gray-500 blk:text-gray-300'}`)}>{moveHereDisabled ? 'View only' : 'Move here'}</Text>
           </TouchableOpacity>
         </View>
       </React.Fragment>
